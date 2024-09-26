c306ae1dc477c3df9d6e82c32246ae93c4031d3e
Pratik <pratik232006@gmail.com>
2024-09-26 20:47:18 +0530

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../Button/Button";
 import Search from "../Search/Search";
 import styles from './Navbar.module.css'
-import LogoImage from "../../Assests/logo.png"
+import LogoImage from "./../../Assets/logo.png";
 
 
 export default function Navbar(){
